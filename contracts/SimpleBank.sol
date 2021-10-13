3d7ccd8d7863f078cbc1d8f8f778efdab7833d05
Didier Collette <didier.collette.s@gmail.com>
2021-10-13 12:07:47 -0400

    Final

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -45,9 +45,9 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract    
-    // function () external payable {
-    //     revert();
-    // }
+    function () external payable {
+        revert();
+    }
 
     /// @notice Get balance
     /// @return The balance of the user
